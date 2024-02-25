b81c7cac275a35659d746d3437ec7b0e0529b194
Ivan Gomez <ivanfedericosimari@gmail.com>
2024-02-25 01:56:52 -0300

    Realizando cambios en el proyecto

--- PreEntrega2-Gomez/src/main.jsx
+++ PreEntrega2-Gomez/src/main.jsx
@@ -2,5 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCNsAPtmOp9NR-0vuf7cl5dl3bdS5qM46w",
+  authDomain: "proyectofinal-ivangomez.firebaseapp.com",
+  projectId: "proyectofinal-ivangomez",
+  storageBucket: "proyectofinal-ivangomez.appspot.com",
+  messagingSenderId: "376906819804",
+  appId: "1:376906819804:web:31185cd62328446793ac19"
+};
+
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
